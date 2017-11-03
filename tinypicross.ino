65be2ae32d78846f8507baec6333e69b333e7d9d
Evan Barger <epbarger@gmail.com>
2017-11-02 21:23:19 -0400

    hints working, playable! except random puzzles can be unsolvable :(

--- tinypicross.ino
+++ tinypicross.ino
@@ -108,7 +108,9 @@ void updateGrid(){
     if (cursorCell()->state == CS_FILL){
       cursorCell()->state = CS_EMPTY;
     } else {
-      cursorCell()->state = CS_FILL;
+      if (gamePuzzle.cellFilled[gameGrid.cursorX][gameGrid.cursorY]) {
+        cursorCell()->state = CS_FILL;
+      }
     }
   }
 }
@@ -132,7 +134,6 @@ void drawGrid(){
     for (byte y = 0; y < GRID_HEIGHT; y++){
       byte drawX = GRID_X_OFFSET + (x * (CELL_SIZE - 1));
       byte drawY = GRID_Y_OFFSET + (y * (CELL_SIZE - 1));
-//      arduboy.drawRect(drawX, drawY, CELL_SIZE, CELL_SIZE, WHITE);
       switch(gameGrid.cells[x][y].state){
         case CS_EMPTY:
           break;
@@ -163,68 +164,118 @@ void drawGrid(){
   byte cursorDrawY = GRID_Y_OFFSET + (gameGrid.cursorY * (CELL_SIZE - 1));
   arduboy.drawRect(cursorDrawX, cursorDrawY, CELL_SIZE, CELL_SIZE, BLACK);
   arduboy.drawRect(cursorDrawX - 1, cursorDrawY - 1, CELL_SIZE + 2, CELL_SIZE + 2, WHITE);
-//  arduboy.drawRect(cursorDrawX - 2, cursorDrawY - 2, CELL_SIZE + 4, CELL_SIZE + 4, WHITE);
 }
 
 void drawHintColumn(byte columnIndex){
   byte drawIndexX = GRID_X_OFFSET + 2 + (columnIndex * (CELL_SIZE - 1));
   byte drawIndexY = GRID_Y_OFFSET - 7;
+  byte zeroHints = 0;
   for (int i = COLUMN_HINT_MAX_NUMS - 1; i >= 0; i--){
     byte hintNum = gamePuzzle.columnHints[columnIndex][i];
     if (hintNum > 0) {
       tinyfont.setCursor(drawIndexX, drawIndexY);
       tinyfont.setTextColor(gameGrid.cursorX != columnIndex);
-//      tinyfont.setTextBackground(gameGrid.cursorx == columnIndex);
       tinyfont.print(String(hintNum));
       drawIndexY -= 6;
+    } else {
+      zeroHints++;
     }
   }
+  if (zeroHints >= COLUMN_HINT_MAX_NUMS){
+    tinyfont.setCursor(drawIndexX, drawIndexY);
+    tinyfont.setTextColor(gameGrid.cursorX != columnIndex);
+    tinyfont.print(F("0"));
+  }
   tinyfont.setTextColor(WHITE);
-//  tinyfont.setTextBackground(BLACK);
 }
 
 void drawHintRow(byte rowIndex){
   byte drawIndexX = GRID_X_OFFSET - 7;
   byte drawIndexY = GRID_Y_OFFSET + 2 + (rowIndex * (CELL_SIZE - 1));
+  byte zeroHints = 0;
   for (int i = ROW_HINT_MAX_NUMS - 1; i >= 0; i--){
     byte hintNum = gamePuzzle.rowHints[rowIndex][i];
     if (hintNum > 0) {
       if (hintNum > 9) { drawIndexX -= 5; }
       tinyfont.setCursor(drawIndexX, drawIndexY);
       tinyfont.setTextColor(gameGrid.cursorY != rowIndex);
-//      tinyfont.setTextBackground(gameGrid.cursorY == rowIndex);
       tinyfont.print(String(hintNum));
       drawIndexX -= 6;
+    } else {
+      zeroHints++;
     }
   }
+  if (zeroHints >= ROW_HINT_MAX_NUMS){
+    tinyfont.setCursor(drawIndexX, drawIndexY);
+    tinyfont.setTextColor(gameGrid.cursorY != rowIndex);
+    tinyfont.print(F("0"));
+  }
   tinyfont.setTextColor(WHITE);
-//  tinyfont.setTextBackground(BLACK);
 }
 
 
 /////////////////// Other /////////////
 
 void initializePuzzle(){
+  Puzzle randomPuzzle = generateRandomPuzzle();
   for (byte x = 0; x < GRID_WIDTH; x++){
     for (byte y = 0; y < GRID_HEIGHT; y++){
-      gamePuzzle.cellFilled[x][y] = puzzle1[x][y];
-      gamePuzzle.rowHints[y][0] = 3;
-      gamePuzzle.rowHints[y][1] = 3;
-      gamePuzzle.rowHints[y][2] = 4;
-      gamePuzzle.rowHints[y][3] = 5;
-      gamePuzzle.rowHints[y][4] = 6;
+      gamePuzzle.cellFilled[x][y] = randomPuzzle.cellFilled[x][y];
+      gamePuzzle.rowHints[y][0] = 0;
+      gamePuzzle.rowHints[y][1] = 0;
+      gamePuzzle.rowHints[y][2] = 0;
+      gamePuzzle.rowHints[y][3] = 0;
+      gamePuzzle.rowHints[y][4] = 0;
     }
-    gamePuzzle.columnHints[x][0] = 1;
-    gamePuzzle.columnHints[x][1] = 3;
-    gamePuzzle.columnHints[x][2] = 2;
+    gamePuzzle.columnHints[x][0] = 0;
+    gamePuzzle.columnHints[x][1] = 0;
+    gamePuzzle.columnHints[x][2] = 0;
   }
+
+  for (byte x = 0; x < GRID_WIDTH; x++){
+    byte hintNum = 0;
+    byte hintIndex = 0;
+    for (byte y = 0; y < GRID_HEIGHT; y++){
+      if (gamePuzzle.cellFilled[x][y]){
+        hintNum++;
+      } else if (hintNum > 0) {
+        gamePuzzle.columnHints[x][hintIndex] = hintNum;
+        hintNum = 0;
+        hintIndex++;
+      }
+
+      if (!(y + 1 < GRID_HEIGHT)){
+        gamePuzzle.columnHints[x][hintIndex] = hintNum;
+      }
+    }
+  }
+
+  
+  for (byte y = 0; y < GRID_HEIGHT; y++){
+    byte hintNum = 0;
+    byte hintIndex = 0;
+    for (byte x = 0; x < GRID_WIDTH; x++){
+      if (gamePuzzle.cellFilled[x][y]){
+        hintNum++;
+      } else if (hintNum > 0) {
+        gamePuzzle.rowHints[y][hintIndex] = hintNum;
+        hintNum = 0;
+        hintIndex++;
+      }
+
+      if (!(x + 1 < GRID_WIDTH)){
+        gamePuzzle.rowHints[y][hintIndex] = hintNum;
+      }
+    }
+  }
+  
 }
 
 void initializeGrid(){
   for (byte x = 0; x < GRID_WIDTH; x++){
     for (byte y = 0; y < GRID_HEIGHT; y++){
       Cell newCell;
-      newCell.state = CS_EMPTY;
+      newCell.state = CS_EMPTY; // gamePuzzle.cellFilled[x][y]; 
       gameGrid.cells[x][y] = newCell;
     }
   }
@@ -237,6 +288,32 @@ Cell* cursorCell(){
 }
 
 int modulo(int x, int y){
-   return x < 0 ? ((x + 1) % y) + y - 1 : x % y;
+  return x < 0 ? ((x + 1) % y) + y - 1 : x % y;
+}
+
+Puzzle generateRandomPuzzle(){
+  Puzzle randomPuzzle;
+  for (byte x = 0; x < GRID_WIDTH; x++){
+    for (byte y = 0; y < GRID_HEIGHT; y++){
+      randomPuzzle.cellFilled[x][y] = false;
+    }
+  }
+  
+  for (byte y = 0; y < GRID_HEIGHT; y++){
+    byte remainingCellIndicies[GRID_WIDTH] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+    byte arrayLength = GRID_WIDTH;
+    byte numToFill = random(0, GRID_WIDTH + 1); 
+    while (numToFill > 0){
+      byte chosenIndex = remainingCellIndicies[random(0, arrayLength)];
+      randomPuzzle.cellFilled[chosenIndex][y] = true;
+      arrayLength--;
+      for (byte i = chosenIndex; i < arrayLength; i++){
+        remainingCellIndicies[i] = remainingCellIndicies[i+1];
+      }
+      numToFill--;
+    }
+  }
+  
+  return randomPuzzle;
 }
 
